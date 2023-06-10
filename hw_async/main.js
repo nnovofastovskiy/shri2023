fcb0241952ab19639a5a194ecefd1fbd9088548b
Nikita <bananchik26@gmail.com>
2023-06-10 23:52:18 +0300

    first ok

---
+++ hw_async/main.js
@@ -0,0 +1,86 @@
+async function solution({ minPrice, maxPrice, catalog }) {
+    let result = [];
+
+    let catalogStack = [catalog];
+    let productStack = [];
+
+    while (catalogStack.length > 0) {
+        const cat = await getAllFromCatalog(catalogStack.pop());
+        const isActive = cat[1];
+        const children = cat[0];
+        if (isActive) {
+            for (let i = children.length - 1; i >= 0; i--) {
+                const child = children[i];
+                if (child.hasOwnProperty('getChildren')) {
+                    catalogStack.push(child);
+                } else {
+                    productStack.push(child)
+                }
+            }
+        }
+    }
+
+    return await Promise.all(productStack.map(prod => getAllFromProduct(prod))).then(res => {
+        return res
+            .filter(prod => prod[0] && (prod[2] >= minPrice && prod[2] <= maxPrice))
+            .sort((p1, p2) => {
+                if (p1[2] == p2[2]) {
+                    return p1[1].localeCompare(p2[1])
+                }
+                return p1[2] - p2[2]
+            })
+            .map(prod => {
+                return { name: prod[1], price: prod[2] }
+            });
+    })
+}
+
+const getIsActive = function (item) {
+    return new Promise((resolve, reject) => {
+        item.checkIsActive((error, isActive) => {
+            if (error) resolve(getIsActive(item));
+            else resolve(isActive);
+        })
+    })
+}
+
+const getChildren = function (catalog) {
+    return new Promise((resolve, reject) => {
+        catalog.getChildren((error, children) => {
+            if (error) resolve(getChildren(catalog));
+            else resolve(children);
+        });
+    })
+}
+
+const getName = function (item) {
+    return new Promise((resolve, reject) => {
+        item.getName((error, name) => {
+            if (error) resolve(getName(item));
+            else resolve(name);
+        })
+    })
+}
+
+const getPrice = function (item) {
+    return new Promise((resolve, reject) => {
+        item.getPrice((error, price) => {
+            if (error) resolve(getPrice(item));
+            else resolve(price);
+        })
+    })
+}
+
+const getAllFromCatalog = async (catalog) => {
+    return Promise.all([getChildren(catalog), getIsActive(catalog), getName(catalog)]).then(res => {
+        return res;
+    })
+}
+
+const getAllFromProduct = async (product) => {
+    return Promise.all([getIsActive(product), getName(product), getPrice(product)]).then(res => {
+        return res;
+    })
+}
+
+module.exports = solution;
--- hw_async/playground.js
+++ hw_async/playground.js
@@ -11,7 +11,7 @@
 
     const addRandomError = (fn, result) => {
         return () => {
-            const isError = Math.random() <= 0.8;
+            const isError = Math.random() <= 0.2;
 
             if (isError) {
                 fn(new Error('Something went wrong'), null);
@@ -63,61 +63,88 @@
 
 // решение задачи
 async function solution({ minPrice, maxPrice, catalog }) {
-    const result = [];
+    let result = [];
 
-    // function promisify(f) {
-    //     return function (...args) { // возвращает функцию-обёртку
-    //         return new Promise((resolve, reject) => {
-    //             function callback(err, result) { // наш специальный колбэк для f
-    //                 if (err) {
-    //                     reject(err);
-    //                 } else {
-    //                     resolve(result);
-    //                 }
-    //             }
+    let catalogStack = [catalog];
+    let productStack = [];
 
-    //             args.push(callback); // добавляем колбэк в конец аргументов f
-
-    //             f.call(this, ...args); // вызываем оригинальную функцию
-    //         });
-    //     };
-    // };
-
-    // примеры вызова методов
-    // catalog.checkIsActive((error, isActive) => console.log({ error, isActive }));
-    // catalog.getName((error, name) => console.log({ error, name }));
-    let cnt = 0;
-    const getChildren = function () {
-        console.log('cnt = ', cnt++)
-        return new Promise((resolve, reject) => {
-            catalog.getChildren((error, children) => {
-                // console.log({ error, children });
-                if (error) {
-                    resolve(getChildren());
+    while (catalogStack.length > 0) {
+        const cat = await getAllFromCatalog(catalogStack.pop());
+        const isActive = cat[1];
+        const children = cat[0];
+        if (isActive) {
+            for (let i = children.length - 1; i >= 0; i--) {
+                const child = children[i];
+                if (child.hasOwnProperty('getChildren')) {
+                    catalogStack.push(child);
+                } else {
+                    productStack.push(child)
                 }
-                else resolve(children);
-            });
-        })
+            }
+        }
     }
 
-    const children = await getChildren();
-    console.log(children);
+    return await Promise.all(productStack.map(prod => getAllFromProduct(prod))).then(res => {
+        return res
+            .filter(prod => prod[0] && (prod[2] >= minPrice && prod[2] <= maxPrice))
+            .sort((p1, p2) => {
+                if (p1[2] == p2[2]) {
+                    return p1[1].localeCompare(p2[1])
+                }
+                return p1[2] - p2[2]
+            })
+            .map(prod => {
+                return { name: prod[1], price: prod[2] }
+            });
+    })
+}
 
+const getIsActive = function (item) {
+    return new Promise((resolve, reject) => {
+        item.checkIsActive((error, isActive) => {
+            if (error) resolve(getIsActive(item));
+            else resolve(isActive);
+        })
+    })
+}
 
-    // const children = await Promise.resolve(catalog.getChildren((error, children) => { return { error, children } }));
-    // console.log(await children)
-    // catalog.getChildren();
+const getChildren = function (catalog) {
+    return new Promise((resolve, reject) => {
+        catalog.getChildren((error, children) => {
+            if (error) resolve(getChildren(catalog));
+            else resolve(children);
+        });
+    })
+}
 
-    // const children = catalog.getChildren((error, children) => {
-    //     return new Promise((resolve, reject) => {
-    //         if (error) reject(error)
-    //         // console.log(children);
-    //         resolve(children)
-    //     })
-    // });
-    // console.log(await children);
+const getName = function (item) {
+    return new Promise((resolve, reject) => {
+        item.getName((error, name) => {
+            if (error) resolve(getName(item));
+            else resolve(name);
+        })
+    })
+}
 
-    return result;
+const getPrice = function (item) {
+    return new Promise((resolve, reject) => {
+        item.getPrice((error, price) => {
+            if (error) resolve(getPrice(item));
+            else resolve(price);
+        })
+    })
+}
+
+const getAllFromCatalog = async (catalog) => {
+    return Promise.all([getChildren(catalog), getIsActive(catalog), getName(catalog)]).then(res => {
+        return res;
+    })
+}
+
+const getAllFromProduct = async (product) => {
+    return Promise.all([getIsActive(product), getName(product), getPrice(product)]).then(res => {
+        return res;
+    })
 }
 
 module.exports = solution;
