0accf6c48a2ab7e6de8910ba71b37a86c6ae4b0c
Nikita <bananchik26@gmail.com>
2023-06-11 02:06:17 +0300

    work 1

--- algorithms/C/C.js
+++ algorithms/C/C.js
@@ -9,9 +9,27 @@ console.log('arr = ', arr);
 
 function solution(n, arr) {
     if (n == 1) return '0 0';
-
+    let maxIndex = n - 1;
+    let minIndex = n - 2;
+    let max = arr[maxIndex];
+    let diff = 0;
+    for (let i = minIndex; i >= 0; i--) {
+        console.log('max = ', max);
+        console.log('arr[i] = ', arr[i]);
+        if (arr[i] >= max) {
+            max = arr[i];
+            maxIndex = i;
+        }
+        else {
+            const newDiff = max - arr[i];
+            if (newDiff >= diff) {
+                minIndex = i;
+                diff = newDiff;
+            }
+        }
+    }
+    if (minIndex == maxIndex) return '0 0'
+    return `${minIndex + 1} ${maxIndex + 1}`;
 }
 
-// console.log(solution())
-
-// fs.writeFileSync("output.txt", solution(n, arr).toString())
+fs.writeFileSync("output.txt", solution(n, arr).toString())
